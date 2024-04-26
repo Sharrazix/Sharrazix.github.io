40fb1ac26cbd124016a479c63ea88de3e2451859
Sharrazix <132606180+Sharrazix@users.noreply.github.com>
2024-04-26 16:26:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
-	<title>Document</title>
-</head>
-<body>
-	<div class="container">
-		<div class="inner">
-			<div class="item" id="item1">
-				<img src="1.png" alt="" class="img">
-				<button class="btn" id="btn1"><b>Add</b></button>
-			</div>
-			<div class="item" id="item2">
-				<img src="1.png" alt="" class="img">
-				<button class="btn" id="btn2"><b>Add</b></button>
-			</div>
-			<div class="item" id="item3">
-				<img src="1.png" alt="" class="img">
-				<button class="btn" id="btn3"><b>Add</b></button>
-			</div>
-			<div class="item" id="item4">
-				<img src="1.png" alt="" class="img">
-				<button class="btn" id="btn4"><b>Add</b></button>
-			</div>
-			<div class="item" id="item5">
-				<img src="1.png" alt="" class="img">
-				<button class="btn" id="btn5"><b>Add</b></button>
-			</div>
-			<div class="item" id="item6">
-				<img src="1.png" alt="" class="img">
-				<button class="btn" id="btn6"><b>Add</b></button>
-			</div>
-		</div>
-	</div>
-	<div class="usercard" id="usercard">
-		
-	</div>
+let tg = window.Telegram.WebApp;
 
-	<script src="https://telegram.org/js/telegram-web-app.js"></script>
-	<script src="app.js"></script>
-</body>
-</html>
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+let products = [
+  { name: "Product 1", price: "$10" },
+  { name: "Product 2", price: "$15" },
+  { name: "Product 3", price: "$20" },
+  { name: "Product 4", price: "$25" },
+  { name: "Product 5", price: "$30" },
+  { name: "Product 6", price: "$35" }
+];
+
+let itemElements = document.querySelectorAll(".item");
+
+itemElements.forEach((itemElement, index) => {
+  let btn = itemElement.querySelector(".btn");
+  let product = products[index];
+  let productName = product.name;
+  let productPrice = product.price;
+
+  let productNameDiv = document.createElement("div");
+  productNameDiv.innerText = `${productName}\n${productPrice}`;
+  productNameDiv.classList.add("product-info");
+
+  itemElement.insertBefore(productNameDiv, btn);
+
+  btn.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+      tg.MainButton.hide();
+    }
+    else {
+      tg.MainButton.setText(`Вы выбрали ${productName}!`);
+      item = `${index + 1}`;
+      tg.MainButton.show();
+    }
+  });
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+  tg.sendData(item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+usercard.appendChild(p);
